a8a514b797451574eddd872dac6526b830d526fd
byron0817 <144976054+byron0817@users.noreply.github.com>
2023-12-07 00:11:56 -0600

    Update movement.java

--- caso2/src/caso2/movement.java
+++ caso2/src/caso2/movement.java
@@ -9,5 +9,58 @@ package caso2;
  * @author sombr
  */
 public class movement {
+    public class movement {
+    private String descripcion;
+    private friends[] participantes;
+    private friends quienPago;
+    private double montoTotal;
+    private String moneda;
+
+    public movement(String descripcion, friends[] participantes, friends quienPago, int montoTotal, String moneda) {
+        this.descripcion = descripcion;
+        this.participantes = participantes;
+        this.quienPago = quienPago;
+        this.montoTotal = montoTotal;
+        this.moneda = moneda;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public friends[] getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(friends[] participantes) {
+        this.participantes = participantes;
+    }
+
+    public friends getQuienPago() {
+        return quienPago;
+    }
+
+    public void setQuienPago(firnds quienPago) {
+        this.quienPago = quienPago;
+    }
+
+    public double getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
     
 }
