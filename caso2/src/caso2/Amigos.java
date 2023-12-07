7d1fafa0e283ffba88bb76af0cdf43b916e77bfc
byron0817 <144976054+byron0817@users.noreply.github.com>
2023-12-07 00:12:59 -0600

    Update Amigos.java

--- caso2/src/caso2/Amigos.java
+++ caso2/src/caso2/Amigos.java
@@ -9,5 +9,37 @@ package caso2;
  * @author sombr
  */
 public class Amigos {
+    public String nombre;
+    public double montDebe;
+    public double montCobrar;
+
+    public friends (String nombre) {
+        this.nombre = nombre;
+        this.montDebe = 0.0;
+        this.montCobrar = 0.0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getMontDebe() {
+        return montDebe;
+    }
+
+    public void setMontDebe(double montDebe) {
+        this.montDebe = montDebe;
+    }
+
+    public double getMontCobrar() {
+        return montCobrar;
+    }
+
+    public void setMontCobrar(double montCobrar) {
+        this.montCobrar = montCobrar;
     
 }
